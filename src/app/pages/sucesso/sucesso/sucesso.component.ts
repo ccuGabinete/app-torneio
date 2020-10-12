c910a49edda06e4c7d91c5892de04e55bcdf85d8
kakalima76 <nieraldo@gmail.com>
2020-10-12 15:36:51 -0300

    v0.0.20

--- src/app/pages/sucesso/sucesso/sucesso.component.ts
+++ src/app/pages/sucesso/sucesso/sucesso.component.ts
@@ -1,10 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Usuario } from 'src/app/models/usuario/usuario';
-import { CadastroService } from 'src/app/services/cadastro/cadastro.service';
-import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
-import { LogadoService } from 'src/app/services/logado/logado.service';
-import { data } from 'jquery';
 
 @Component({
   selector: 'app-sucesso',
@@ -12,26 +6,10 @@ import { data } from 'jquery';
   styleUrls: ['./sucesso.component.css']
 })
 export class SucessoComponent implements OnInit {
-  mostrar: boolean;
 
-  constructor(
-    private cadastroservice: CadastroService,
-    private router: Router,
-    public logado: Usuario,
-    private logadoservice: LogadoService
-  ) { }
+  constructor() { }
 
-  async ngOnInit(): Promise<void> {
-    this.logadoservice.currentMessage.subscribe(data => {
-      this.logado = data;
-      this.cadastroservice.buscarEmail(this.logado.email).subscribe(data => {
-        if (data.body[0].Total > 0) {
-          this.router.navigate(['/']);
-        } else {
-          this.router.navigate(['/cadastro']);
-        }
-      })
-    })
+  ngOnInit(): void {
   }
 
 }
